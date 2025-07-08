162c4313f864e2590d2866890e27d0ca9966f780
yekshithkadhati <22211a6750@bvrit.ac.in>
2025-07-08 11:51:49 +0530

    Connected all modules in health.py

--- health.py
+++ health.py
@@ -1,1 +1,17 @@
- 
+from monitor.monitor import get_service_metrics
+from predictor.predictor import predict_failure
+from healer.healer import decide_healing_action
+from orchestrator.orchestrator import execute_action
+def run_health_orchestration(service_name):
+    metrics = get_service_metrics(service_name)
+    print(f"Collected Metrics: {metrics}")
+
+    if predict_failure(**metrics):
+        print("Failure predicted.")
+        action = decide_healing_action(service_name, metrics)
+        execute_action(service_name, action)
+    else:
+        print("Service is healthy. No action needed.")
+if __name__ == "__main__":
+    run_health_orchestration("auth-service")
+
