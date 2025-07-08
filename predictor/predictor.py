2536bfdba15e61b09f2d48e0a8cbdce0d96c1b02
yekshithkadhati <22211a6750@bvrit.ac.in>
2025-07-08 11:45:05 +0530

    Added predictor module

--- predictor/predictor.py
+++ predictor/predictor.py
@@ -1,1 +1,8 @@
- 
+def predict_failure(cpu_usage, memory_usage, response_time):
+    if cpu_usage > 85 or memory_usage > 90 or response_time > 900:
+        return True
+    return False
+
+if __name__ == "__main__":
+    test = predict_failure(cpu_usage=91, memory_usage=50, response_time=300)
+    print("Predicted Failure:" if test else "Service is Healthy")
